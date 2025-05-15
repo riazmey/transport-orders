de55b6d10ecb201f1bdcd5deb0518be2e966ac96
Иван Радаев <i.radaev@ltlog.ru>
2025-05-15 13:09:00 +0300

    edit entrypoint - createcachetable

--- entrypoint-app.sh
+++ entrypoint-app.sh
@@ -2,7 +2,6 @@
 #!/bin/sh
 
 python /app/transport-orders/manage.py createcachetable
-python /app/transport-orders/manage.py clearcache
 
 python /app/transport-orders/manage.py runserver 0.0.0.0:${WEB_PORT}
 #gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
